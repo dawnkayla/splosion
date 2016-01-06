db83afffa083b32bb900660821ea7d1bbc9e6dc5
Greg Allan <gregory.j.allan@gmail.com>
2016-01-05 17:43:41 -0800

    parameterize the start and end date values
    - instead of having them hardcoded use today
      and thirty days ago so the analyst has the most
      up to day information

--- js/details.js
+++ js/details.js
@@ -1,21 +1,37 @@
 // TODO handle if not params.company
 // TODO handle if non 200 respsonse from getJSON
 // TODO clean up AKA better param names
-// TODO parameterize startdate/enddate
 // TODO remove yAxis title
 // TODO remove legend
 // TODO add some simple logic for BUY/SELL/WAIT
 // TODO add in some documentation
 // TODO increase BUY/SELL/WAIT font size
+// TODO Change chart label. to company name / stock
 
-var url = window.location.href
-var params = {};
-url.split('?')[1].split('&').forEach(function(string) {
-  var parts = string.split('=');
-  params[parts[0]] = parts[1];
-});
-var tail = params.company + '?startdate=20150501&enddate=20150530'
-$.getJSON("http://stocksplosion.apsis.io/api/company/" + tail)
+var dateOffset = (24 * 60 * 60 * 1000) * 30; //30 days
+var today = new Date();
+var thirtyDaysAgo = new Date(today.getTime() - dateOffset);
+
+var endDate   = today.toISOString().split('T')[0].replace(/-/g, '');
+var startDate = thirtyDaysAgo.toISOString().split('T')[0].replace(/-/g, '');
+
+
+function getAPIURL() {
+  var url = window.location.href
+  var params = {};
+
+  // parse all the url params and put them in an object
+  url.split('?')[1].split('&').forEach(function(string) {
+    var parts = string.split('=');
+    params[parts[0]] = parts[1];
+  });
+
+  var tail = params.company + '?startdate=' + startDate + '&enddate=' + endDate;
+  return "http://stocksplosion.apsis.io/api/company/" + tail;
+}
+
+
+$.getJSON(getAPIURL())
   .done(function(data) {
     $('#foo').text('BUY');
     var goodData = Object.keys(data.prices).map(function(date) {
@@ -32,7 +48,7 @@ $.getJSON("http://stocksplosion.apsis.io/api/company/" + tail)
         type: 'line',
         name: 'price',
         data: goodData,
-        pointStart: Date.UTC(2015, 05, 1),
+        pointStart: thirtyDaysAgo.getTime(),
         pointInterval: 24 * 3600 * 1000
       }],
 
